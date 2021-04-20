714b3d8d1e950cc8dcb19fa6d3750521680f9042
Vishnu-rkumar <82866605+Vishnu-rkumar@users.noreply.github.com>
2021-04-20 12:37:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React        from 'react';
+import Loginform    from './Loginform';
+import './App.css';
+
+
+class App extends React.Component  {
+  render() {
+
+    return (
+       <div className="container">
+          
+          <Loginform />
+        
+      </div>
+    );
+  }
+
+}
+
+export default App;
